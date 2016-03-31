1ab2c09ffe39343106806090cbb541e81f83a627
erichc_li <erichc_li@asus.com>
2016-03-31 15:22:18 +0800

    add M permission

--- app/src/main/java/camera2/erichc_li/com/camera2app/MainActivity.java
+++ app/src/main/java/camera2/erichc_li/com/camera2app/MainActivity.java
@@ -1,10 +1,14 @@
 package camera2.erichc_li.com.camera2app;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.ImageFormat;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -16,20 +20,21 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
 
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
+
     private Camera2Preview mCamera2Preview;
     private FrameLayout mFrameLayout;
 
-    private HandlerThread mCaptureThread;
-    private Handler mCaptureHandler;
+    private View mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mLayout = findViewById(R.id.main_layout);
 
-        initBackThread();
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,34 +48,86 @@ public class MainActivity extends AppCompatActivity {
 
         mFrameLayout = (FrameLayout) findViewById(R.id.camera_textureview);
 
-        mCamera2Preview = new Camera2Preview(this, ImageFormat.JPEG);
-        mFrameLayout.addView(mCamera2Preview);
+        showCameraPreview();
 
-    }
-
-    private void initBackThread()
-    {
-        mCaptureThread = new HandlerThread("check-Capture-Thread");
-        mCaptureThread.start();
-        mCaptureHandler = new Handler(mCaptureThread.getLooper());
-    }
-
-    private void stopBackThread() {
-
-        mCaptureThread.quitSafely();
-        try {
-            mCaptureThread.join();
-            mCaptureThread = null;
-            mCaptureHandler = null;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        stopBackThread();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            // Request for camera permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted. Start camera preview Activity.
+                Snackbar.make(mLayout, "Camera permission was granted. Starting preview.",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+                startCamera();
+            } else {
+                // Permission request was denied.
+                Snackbar.make(mLayout, "Camera permission request was denied.",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    private void showCameraPreview() {
+        // Check if the Camera permission has been granted
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is already available, start camera preview
+            Snackbar.make(mLayout,
+                    "Camera permission is available. Starting preview.",
+                    Snackbar.LENGTH_SHORT).show();
+            startCamera();
+        } else {
+            // Permission is missing and must be requested.
+            requestCameraPermission();
+        }
+    }
+
+    private void startCamera() {
+        mCamera2Preview = new Camera2Preview(this, ImageFormat.JPEG);
+        mFrameLayout.addView(mCamera2Preview);
+    }
+
+    /**
+     * Requests the {@link android.Manifest.permission#CAMERA} permission.
+     * If an additional rationale should be displayed, the user has to launch the request from
+     * a SnackBar that includes additional information.
+     */
+    private void requestCameraPermission() {
+        // Permission has not been granted and must be requested.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with a button to request the missing permission.
+            Snackbar.make(mLayout, "Camera access is required to display the camera preview.",
+                    Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            PERMISSION_REQUEST_CAMERA);
+                }
+            }).show();
+
+        } else {
+            Snackbar.make(mLayout,
+                    "Permission is not available. Requesting camera permission.",
+                    Snackbar.LENGTH_SHORT).show();
+            // Request the permission. The result will be received in onRequestPermissionResult().
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    PERMISSION_REQUEST_CAMERA);
+        }
     }
 
     @Override
